e7cb6b64b6850a35ccae8d38acca94ad470b47f5
darinanosonova <ms.dnosonova@mail.ru>
2021-11-24 14:06:25 +0300

    Help

---
+++ Calculator/Google Test/test.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "..\Project3\TCalculator.h"
+
+TEST(TestCaseName, TestName) {
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+};
+
+TEST(TestCaseName, TestName) {
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+};
